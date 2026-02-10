8cbb0153af201400bb691b5960f8afd58f9ef9bb
tapion69 <151565403+tapion69@users.noreply.github.com>
2026-02-10 19:50:57 +0100

    Update settings.js

--- smart-voltronic/settings.js
+++ smart-voltronic/settings.js
@@ -2,8 +2,12 @@ module.exports = {
   uiPort: 1880,
   userDir: "/data",
 
-  // ⬇️ IMPORTANT : chemin absolu
+  // IMPORTANT : chemin absolu
   flowFile: "/data/flows.json",
+
+  // ✅ Rend le chiffrement des credentials STABLE entre redémarrages
+  // Mets une valeur unique à toi (longue) pour éviter tout souci
+  credentialSecret: "CHANGE_ME__smart_voltronic__long_random_secret",
 
   nodesDir: ["/opt/node_modules"],
 
