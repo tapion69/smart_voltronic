81992f3e33cca35cb17517574e9565d1e55c299d
tapion69 <151565403+tapion69@users.noreply.github.com>
2026-02-10 18:26:23 +0100

    Update settings.js

--- smart-voltronic/settings.js
+++ smart-voltronic/settings.js
@@ -1,17 +1,11 @@
 module.exports = {
-  // Port UI (ok)
   uiPort: 1880,
-
-  // IMPORTANT: répertoire persistant HA (volume)
   userDir: "/data",
 
-  // Ton fichier de flows (tu peux garder flows.json si tu préfères)
-  flowFile: "flows.json",
+  // ⬇️ IMPORTANT : chemin absolu
+  flowFile: "/data/flows.json",
 
-  // IMPORTANT: charge aussi les nodes installés dans l'image (ex: serialport)
-  nodesDir: [
-    "/opt/node_modules"
-  ],
+  nodesDir: ["/opt/node_modules"],
 
   editorTheme: {
     projects: { enabled: false }
